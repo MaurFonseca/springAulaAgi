880723db01aaabe7f93e084f17e0d9e9111a1e0a
Mauricio Zazeri Fonseca <maurz.fonseca@gmail.com>
2025-09-18 14:25:30 -0300

    Aplicações levadas para as pastas corretas

---
+++ src/main/java/com/devagi/meuprojeto/SpringAulaAgiApplication.java
@@ -0,0 +1,13 @@
+package com.devagi.meuprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAulaAgiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAulaAgiApplication.class, args);
+	}
+
+}
